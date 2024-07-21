89ae275eb0085a6ef197d7a8973c52695c9a7f5b
Rayen Elhafsi <Elhafsirayen@ieee.org>
2024-07-21 12:13:50 +0100

    first steps creating DB with postgresql and linking with backend using Django . i run the server and boom it worked after fixing errors .. thank god lets continue

---
+++ glsi/Sem1/venv/file_management/files/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+# Create your views here.
+from django.shortcuts import render, redirect
+from .models import File
+from .forms import FileForm
+
+def file_list(request):
+    files = File.objects.all()
+    return render(request, 'files/file_list.html', {'files': files})
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = FileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('file_list')
+    else:
+        form = FileForm()
+    return render(request, 'files/upload_file.html', {'form': form})
